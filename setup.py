f4d13e89a4d921eced94f2b64a8e4a5fd403130f
floramouj <61322652+floramouj@users.noreply.github.com>
2022-07-11 11:16:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="TemplatePythonPackage",
     version="0.1.0",
     description="A Minimal Template Python Package",
-    url="https://github.com/RealityBending/TemplatePythonPackage",
+    url="https://github.com/floramouj/templatepythonpackage",
     author="Dominique Makowski",
     author_email="dom.makowski@gmail.com",
     license="MIT",
